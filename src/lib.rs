4278b767e26f72649ca4201bc60fa2c5ec69a215
TheLazyFerret <150300262+TheLazyFerret@users.noreply.github.com>
2025-08-10 11:58:29 +0100

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+//! Author: TheLazyFerret (https://github.com/TheLazyFerret)
+//! Copyright (c) 2025 TheLazyFerret
+//! Licensed under the MIT License. See LICENSE file in the project root for full license information.
+//!
+//! Main lib file
+
+pub struct Graph {
+  
+}
