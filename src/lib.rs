230a0ff78c3d0932cd7585d5b7f1b879175aee4a
TheLazyFerret <150300262+TheLazyFerret@users.noreply.github.com>
2025-08-11 00:21:54 +0100

    Basic structs, Default and new for Graph

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,33 @@
 //!
 //! Main lib file
 
+use core::fmt;
+
+// (destiny, weight)
+type Edge = (u8, i32);
+
+#[derive(Default)]
 pub struct Graph {
-  
+  adjacency_list: Vec<Vec<Edge>>,
+  is_directed: bool,
+}
+
+impl Graph {
+  /// Creates a new instance of the struct
+  pub fn new(is_directed: bool) -> Self {
+    Graph {
+      is_directed,
+      ..Default::default()
+    }
+  }
+} // impl Graph
+
+/// Enum representing all possible errors
+#[derive(Clone, Copy, PartialEq, Eq)]
+pub enum Error {}
+
+impl fmt::Display for Error {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    todo!()
+  }
 }
